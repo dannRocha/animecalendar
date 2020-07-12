b6cafc47607f39116f24b98042f16debaf4b1783
Daniel Rocha <rochadaniel15@gmail.com>
2020-07-12 18:06:36 -0300

    Implementado o cabeçalho

--- src/pages/components/Header/Header.js
+++ src/pages/components/Header/Header.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
+
 const Header = styled.header`
-    background: red;
-    width: 100%;
+    background: var(--_2-color);
+    text-align: center;
+    display: flex;
+    justify-content: space-between;
+    color: var(--_3-color);
+    date {
+        font-weight: bold;
+        font-size: 12pt;    
+    }
+
+    h1 {
+        justify-content: center;
+        align-content: center;
+        text-align: center;
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        font-size: 50pt;
+    }
+
 `
 export default function ({ title }){
+    
     return (
         <Header>
             <h1>{ title ?? 'Header' }</h1>
+            <date>{ new Date().toLocaleDateString() }</date>
         </Header>
     )
 }
