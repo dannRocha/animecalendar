5be38e6919182180649ff59b7f4721ef1b6b5eac
Daniel Rocha <rochadaniel15@gmail.com>
2020-07-15 02:01:54 -0300

    Pequenos ajuste de CSS nas paginas

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,12 +4,21 @@ import Header from '../components/Header/Header.js'
 import Main from '../components/Main/Main.js'
 import BarBottom from '../components/BarBottom/BarBottom.js'
 
+
 function Home() {
+    
+    const date = {
+        format: new Date().toDateString().slice(0, 3),
+        toString() { 
+            return this.format 
+        }
+    }
+    
     return (
         <Container>
             <Header title = 'Home'/>
             <Main 
-                subTitle = { `${new Date().toDateString().slice(0, 3) } List` } 
+                subTitle = {<time> { `${date} List` }</time> }
             />
             <BarBottom />
         </Container>
--- src/pages/overviewAnime/OverviewAnime.js
+++ src/pages/overviewAnime/OverviewAnime.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from '../../components/container/Container.js'
 import Header from '../components/Header/Header.js'
 import BarBottom from '../components/BarBottom/BarBottom.js'
 import styled from 'styled-components'
+import { FiHeart } from 'react-icons/fi'
 
 const OverViewAnime =  styled.main`
     display: grid;
     grid-template-columns: 100%;
     grid-template-rows: auto;
     overflow-y: auto;
+
+    position: relative;
     
     figure {
         margin: 0;
@@ -18,6 +21,7 @@ const OverViewAnime =  styled.main`
         img {
             width: 100%;
             height: 100%;
+            object-fit: cover;
         }
 
         figcaption {
@@ -43,6 +47,8 @@ const OverViewAnime =  styled.main`
     }
 
     section {
+        background: var(--_1-color);
+
         header {
             h1 {
                 font-size: 20pt;
@@ -79,6 +85,7 @@ const OverViewAnime =  styled.main`
                 summary {
                     font-weight: bold;
                 }
+                
                 p {
                     text-indent: 20pt;
                     text-align: justify;
@@ -86,21 +93,48 @@ const OverViewAnime =  styled.main`
                 }
             }
         }
-
-        background: var(--_1-color);
     }
 `
 
-export default function ( props ) {
+const FavoriteIcon =  styled( FiHeart )`
+    
+    position: fixed;
+    top: 20%;
+    right: 8%;
+    margin: 0;
+    padding: 0;
+    font-size: 40pt;
+    color: var(--_2-color);
+    fill: ${ props => ( props?.enable === 'true' ) ? '#a00' : '#0000' };
+
+    :hover {
+        cursor: pointer;
+        fill: var(--_2-color);
+    }
+`
+
+export default function( props ) {
    
     const { anime } = props?.location 
+    const [ statusFavoriteIcon, setStatusFavoriteIcon ] = useState('false');
+    
 
+    useEffect( () => {
+        console.log( 'Novo estado do Icone: ', statusFavoriteIcon )
+    
+    }, [ statusFavoriteIcon ] )
+    
+    
     return (
         <Container>
-            <Header title = 'Anime' />
+            <Header title = 'Details' />
             <OverViewAnime>
                 <figure>
-                    <img src = { anime?.image_url } />
+                    <img 
+                        src = {anime?.image_url} 
+                        alt = {`Cover: ${ anime?.title }`}
+                        loading = 'lazy'
+                    />
                     <figcaption >Cover: {anime?.title}</figcaption>
                 </figure>
                 <section>
@@ -109,17 +143,44 @@ export default function ( props ) {
                     </header>
                     <section>
                         <ul>
-                            <li className='topics' >Episodes: {anime?.episodes} </li>
-                            <li className='topics' >Genres: {anime?.genres.map(genre => <span className='tag'>#{ genre.name }</span>)}</li>
-                            <li className='topics' >Source: {anime?.source}</li>
-                            <li className='topics' >Day: {new Date(anime?.airing_start).toDateString().slice(0, 3)} </li>
-                            <li className='topics' >Producers: {anime?.producers.map(producer => <span className='tag'>#{ producer.name }</span>)} </li>
+                            <li className='topics' > Episodes: {anime?.episodes} </li>
+                            <li className='topics' >
+                                Genres: 
+                                {
+                                     anime?.genres.map(( genre, index ) => 
+                                        <span 
+                                            key = { `_@${anime?.image_url}${genre?.name }` } 
+                                            className='tag' >#{ genre?.name } 
+                                        </span>
+                                    )
+                                }
+                            </li>
+                            <li className='topics' > Source: {anime?.source} </li>
+                            <li className='topics' >
+                                Day: { new Date(anime?.airing_start).toDateString().slice(0, 3) } 
+                            </li>
+                            <li className='topics' >
+                                Producers: 
+                                {
+                                    anime?.producers.map(producer => 
+                                        <span 
+                                            key = {`_@${ anime?.image_url }${ producer.name }`} 
+                                            className='tag'>#{ producer.name }
+                                        </span> )
+                                } 
+                            </li>
                         </ul>
                         <details>
                             <summary>Synopsis</summary>
                             <p> {anime?.synopsis}</p>                                
                         </details>
                     </section>
+                    <FavoriteIcon 
+                        enable = { statusFavoriteIcon }
+                        onClick = { () => 
+                            setStatusFavoriteIcon( `${ !(statusFavoriteIcon === 'true')}` )
+                        } 
+                    />
                 </section>
             </OverViewAnime>
             <BarBottom />
