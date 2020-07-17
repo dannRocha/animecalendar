1f2092427a0ae303150b8c5ddacefa9455e479c7
Daniel Rocha <rochadaniel15@gmail.com>
2020-07-16 23:51:55 -0300

    Adicionado Icone de vazio para os favoritos

--- src/pages/bookmarks/Bookmarks.js
+++ src/pages/bookmarks/Bookmarks.js
@@ -1,14 +1,56 @@
 import React from 'react'
 import Container from '../../components/container/Container.js'
 import Header from '../components/Header/Header.js'
-import Main from '../components/Main/Main.js'
 import BarBottom from '../components/BarBottom/BarBottom.js'
+import { MdLayersClear } from 'react-icons/md';
+import styled from 'styled-components'
+
+
+const BookmarkList = styled.section`
+    display: flex;
+    background: var(--_1-color);
+    justify-content: center;
+    align-items: center;
+`
+
+const EmptyStyled = styled.section`
+    
+    display: flex;
+    background: var(--_1-color);
+    justify-content: center;
+    align-items: center;
+    color: var(--_3-color);
+    flex-direction: column;
+    
+    svg {
+        font-size: 80pt;
+    }
+    
+`
+
+
+
+function Empty(){
+    return (
+        <EmptyStyled>
+            <MdLayersClear />
+            <h2>Empty</h2>
+        </EmptyStyled>
+    )
+}
 
 function Bookmarks() {
+
+    let anime = []
+
     return (
         <Container>
             <Header title = 'Bookmarks' />
-            <Main subTitle = {'List'}/>
+            {
+                ( !!anime.length ) 
+                    ? <BookmarkList data = { anime } />
+                    : <Empty />
+            }
             <BarBottom />
         </Container>
     )
