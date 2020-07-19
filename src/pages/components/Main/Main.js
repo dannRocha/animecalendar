0d963579b1af870e997b86af933948aee470414c
Daniel Rocha <rochadaniel15@gmail.com>
2020-07-18 23:58:05 -0300

    Ligação entre o 'core' da aplicação com a interface

--- src/pages/components/Main/Main.js
+++ src/pages/components/Main/Main.js
@@ -4,48 +4,8 @@ import Main from './style.js'
 
 
 export default function ({ animes, subTitle }){
+     
     
-    animes = [
-        {
-            url: "https://myanimelist.net/anime/41353/The_God_of_High_School",
-            title: "The God of High School",
-            image_url: "https://cdn.myanimelist.net/images/anime/1722/107269.jpg",
-            episodes: 13,
-            mal_id: 41353,
-            genres: [
-                { name: "Action" },
-                { name: "Adventure"},
-                { name: "Comedy" },
-            ],
-            synopsis: "It all began as a fighting tournament to seek out for the best fighter among all high school students in Korea. Mori Jin, a Taekwondo specialist and a high school student, soon learns that there is something much greater beneath the stage of the tournament.\r\n\r\n(Source: LINE Webtoon)",
-            producers: [
-                {name: 'MAPPA'},
-            ],
-    
-            source: 'Web manga',
-            airing_start: "2020-07-06T14:30:00+00:00"
-        },
-        {
-            url: "https://myanimelist.net/anime/41353/The_God_of_High_School",
-            title: "The God of High School",
-            image_url: "https://cdn.myanimelist.net/images/anime/1722/107269.jpg",
-            episodes: 13,
-            mal_id: 41353,
-            genres: [
-                { name: "Action" },
-                { name: "Adventure"},
-                { name: "Comedy" },
-            ],
-            synopsis: "It all began as a fighting tournament to seek out for the best fighter among all high school students in Korea. Mori Jin, a Taekwondo specialist and a high school student, soon learns that there is something much greater beneath the stage of the tournament.\r\n\r\n(Source: LINE Webtoon)",
-            producers: [
-                {name: 'MAPPA'},
-            ],
-    
-            source: 'Web manga',
-            airing_start: "2020-07-06T14:30:00+00:00"
-        },
-    ]
-
     return (
         <Main>
             <header> {subTitle ?? 'subTitle'}</header>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from '../../components/container/Container.js'
 import Header from '../components/Header/Header.js'
 import Main from '../components/Main/Main.js'
 import BarBottom from '../components/BarBottom/BarBottom.js'
 
+import store from '../../core/mod.js'
+import Time from '../../core/utils/time/Time.js'
+import Strings from '../../core/utils/strings/Strings.js'
+
 
 function Home() {
     
-    const date = {
-        format: new Date().toDateString().slice(0, 3),
-        toString() { 
-            return this.format 
-        }
-    }
     
+    const [ animeData, setAnimeData ] = useState([])
+
+    
+    useEffect(() => {
+        async function main()
+        {
+            setAnimeData( await store.find( Time.weekday ) )
+        }
+
+        main()
+        
+    }, [] )
+
+
     return (
         <Container>
             <Header title = 'Home'/>
             <Main 
-                subTitle = {<time> { `${date} List` }</time> }
+                subTitle = {
+                    <time> 
+                        { `${ Strings.capitalize( Time.weekday ) } List` }
+                    </time> 
+                }
+                animes = { animeData }
             />
             <BarBottom />
         </Container>
