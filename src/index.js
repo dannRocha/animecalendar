37321076d0b73456514215c48d0341b00b70cf7c
Daniel Rocha <rochadaniel15@gmail.com>
2020-07-22 10:40:50 -0300

    Utilizado estilo global com styled-components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './pages/App.js'
+import GlobalStyle from './components/globalStyle/GlobalStyle.js'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
+      <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
 )
