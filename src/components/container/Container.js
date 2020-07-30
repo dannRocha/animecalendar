e98760cc291d885bd59387078fa4ecae24119618
Daniel Rocha <rochadaniel15@gmail.com>
2020-07-30 16:14:38 -0300

    Adicionado ajuste ao layout para tela em modo paisagem

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -5,8 +5,12 @@ const Container = styled.section`
 
     display: grid;
     grid-template-columns: 100vw;
-    grid-template-rows: 18% 73% 9%;
+    grid-template-rows: 18% auto 9%;
     height: 100vh;
+
+    @media only screen and (max-height: 530px) {    
+        grid-template-rows: 16% auto 13%;
+    }
 `
 
 export default Container
